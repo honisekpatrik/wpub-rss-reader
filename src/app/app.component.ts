1f5c7b102aadb09589b4d86d32325ac167f49f26
Patricius <patrik.honisek565@gmail.com>
2020-04-06 15:15:50 +0200

    fetch and view table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {parseString} from 'xml2js';
+import {FeedReaderService} from './feed-reader.service';
+import {FormControl} from '@angular/forms';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rss-citacka';
+
+  data = false;
+  loading = false;
+
+  xml;
+
+  urlForm = new FormControl('');
+
+  displayedColumns: string[] = ['pubDate', 'title', 'read'];
+  dataSource: MatTableDataSource<any>;
+
+  @ViewChild(MatSort, {static: true}) tableSort: MatSort;
+
+  constructor(private feedService: FeedReaderService) {
+  }
+
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource<any>([]);
+    this.dataSource.sort = this.tableSort;
+    this.dataSource.sortingDataAccessor = (item, property) => {
+      switch (property) {
+        case 'pubDate': return new Date(item.pubDate);
+        default: return item[property];
+      }
+    };
+  }
+
+  getDate(date: string): Date {
+    return new Date(date);
+  }
+
+  loadFeed() {
+    this.loading = true;
+    this.dataSource.data = [];
+    this.data = true;
+
+    this.feedService.readFeed(this.urlForm.value)
+      .subscribe(res => {
+        parseString(res, (err, result) => {
+          this.xml = result;
+        });
+      }, error => {},
+        () => {
+          this.dataSource.data = this.xml.rss.channel[0].item;
+          console.log(this.dataSource.data);
+          this.loading = false;
+        });
+
+  }
+
+  read(element) {
+    console.log(element);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemViewComponent } from './item-view/item-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSortModule} from '@angular/material/sort';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatIconModule,
+    MatSortModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
